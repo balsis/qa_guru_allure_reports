77f821ed0c6e75b5df4d960583044580e94e48e3
artur.balsis <artur.balsis@gmail.com>
2025-03-03 18:28:38 +0300

    add test_github_without_report_step & test_github_with_allure_steps_inside_func

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,12 @@ from selenium import webdriver
 def init_browser():
     options = webdriver.ChromeOptions()
     options.page_load_strategy = 'eager'
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+                         "AppleWebKit/537.36 (KHTML, like Gecko) "
+                         "Chrome/119.0.0.0 Safari/537.36")
     options.add_argument("--window-size=1920,1080")
     # options.add_argument('--headless')
     browser.config.base_url = 'https://github.com'
--- tests/test_github.py
+++ tests/test_github.py
@@ -1,12 +1,37 @@
-from selene import browser
+import allure
+from selene import browser, by, be
 
 
 def test_github_without_report_step():
     browser.open('/')
+    browser.element('.header-search-button').click()
+    browser.element('#query-builder-test').send_keys('eroshenkoam/allure-example').press_enter()
+    browser.element(by.link_text("eroshenkoam/allure-example")).click()
+    browser.element("#issues-tab").click()
+    browser.element(by.text('Тестируем тест')).should(be.clickable)
 
 
+@allure.feature("my_feature")
+@allure.story("my_story")
+@allure.tag("github")
+@allure.link("https://github.com", name = "Testing")
 def test_github_with_allure_steps_inside_func():
-    ...
+    with allure.step(f"Открываем главую страницу GitHub"):
+        browser.open('/')
+    with allure.step("Ищем необходимый репозиторий"):
+        browser.element('.header-search-button').click()
+        browser.element('#query-builder-test').send_keys('eroshenkoam/allure-example').press_enter()
+    with allure.step("Переходим по ссылке в репозиторий"):
+        browser.element(by.link_text("eroshenkoam/allure-example")).click()
+    with allure.step("Открываем таб Issues"):
+        browser.element("#issues-tab").click()
+    with allure.step("Проверяем наличие Issue с названием 'Тестируем тест'"):
+        browser.element(by.text('Тестируем тест')).should(be.clickable)
 
+
+@allure.feature("my_feature")
+@allure.story("my_story")
+@allure.tag("github")
+@allure.link("https://github.com", name = "Testing")
 def test_github_with_allure_steps_using_decorators():
     ...
